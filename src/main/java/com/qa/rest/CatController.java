7c4b5fc7e4498344882100ff0ca951b3f1ece4a7
HPervez@QA.com <Admin@Device>
2021-02-07 01:51:20 +0000

    Updated mappings

--- src/main/java/com/qa/rest/CatController.java
+++ src/main/java/com/qa/rest/CatController.java
@@ -1,21 +1,39 @@
 package com.qa.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.persistence.domain.Cat;
 
 @RestController //this notifies Spring this class is a controller
 @RequestMapping("/cat") //declaring API endpoint
 public class CatController {
 
+	private List<Cat> catList = new ArrayList<>();
+
 	//GET
-	//URL localhost/cat/helloWorld - invokes the helloWorld method within web browser/postman
+	//URL http://localhost:8080/cat/helloWorld - invokes the helloWorld method within web browser/postman
 	@GetMapping("/helloWorld")
-	public String helloWolrd() {
+	public String helloWorld() {
 		return "Hello world"; 		
 	}
 	
-	//POST
+	@GetMapping("/getCats")				
+	public List<Cat> readAll() {
+		return catList;
+	}
+	
+	//POST					//when posting via Postman, must go to Body -> Txt -> Dropdown JSON
+	@PostMapping("/create")
+	public boolean create(@RequestBody Cat cat) {
+		return catList.add(cat);
+	}
 	
 	//PUT
 	
